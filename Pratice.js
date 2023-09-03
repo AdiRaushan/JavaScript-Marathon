b4bd1f955cc185f66076d049b4c99eebe6ec7412
AdiRaushan <adiraushan2k@gmail.com>
2023-09-03 23:51:17 +0530

    improve comment

--- Pratice.js
+++ Pratice.js
@@ -17,5 +17,5 @@ const GetSumOfZero = (arr) => {
 const result = GetSumOfZero([-5, -4, 6, -5, 2, 1, 4, -9, -3, -2]);
 console.log(result);
 
-// Improvised version
+// Improvised version--------
 // ---------------------------------------------------------------------
