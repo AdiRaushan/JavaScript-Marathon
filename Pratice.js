f2d9e142ac924a3b054e35367aff1458e7a57dc9
AdiRaushan <adiraushan2k@gmail.com>
2023-10-03 20:04:33 +0530

    sum

--- Pratice.js
+++ Pratice.js
@@ -1,23 +1,6 @@
-// how to create an array
-
-let myArry = [1, 2, 3, 4, 5, 6, 7, 2];
-
-// hpw to get the lenght of an Array
-
-// let lenght = myArry.length;
-// console.log(lenght);
-
-// Accessing Array Elements: How do you access the first and last elements of an array?
-
-// console.log(myArry[0]);
-
-// let lastIndex = myArry.length - 1;
-// console.log(myArry[lastIndex]);
-
-// Adding Elements: What method do you use to add an element to the end of an array?
-
-let NewEle = [2, 32, 32, 3, 23, 2, 32, 3];
-
-myArry.push(...NewEle);
-
-console.log(myArry);
+const AvgSum = () => {
+  let arr = [12, 12, 1, 2, 12, 1, 21, 2, 12, 12, 1];
+  const sum = arr.reduce((a, b) => a + b) / arr.length;
+  return sum;
+};
+console.log(AvgSum());
