0d90b77af192598f5f9f753dc48e37dbd0313d75
AdiRaushan <adiraushan2k@gmail.com>
2023-10-05 17:01:37 +0530

    question3

---
+++ question3.js
@@ -0,0 +1,25 @@
+// Filter Even Numbers: Write a function that takes an array of numbers and returns a new array with only the even numbers.
+
+/* 
+let a = 6;
+
+if (a % 2 == 0) {
+  console.log("its even");
+} else {
+  console.log("its not");
+}
+ */
+
+/* const EvenNum = () => {
+  let arr = [4, 6, 3, 8, 2, 6, 9, 4, 3, 56, 8, 4, 6, 4, 7, 34, 6, 4, 5];
+  let evenArr = [];
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      //   console.log(arr[i]);
+      evenArr.push(arr[i]);
+    }
+  }
+  return evenArr;
+};
+console.log(EvenNum());
+ */
